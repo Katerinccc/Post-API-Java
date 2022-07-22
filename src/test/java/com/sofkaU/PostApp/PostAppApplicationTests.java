e6eed1c398d14c6ee4a37f47d7218a8383e051b9
Katerin Calderon <kccc0713@gmail.com>
2022-07-21 23:45:52 -0500

    initial project setup

---
+++ src/test/java/com/sofkaU/PostApp/PostAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofkaU.PostApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
