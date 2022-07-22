ffdc1e126e9abd7d4a1ed338ec329281e16416b0
Katerin Calderon <kccc0713@gmail.com>
2022-07-21 23:47:13 -0500

    add entity class Comment and Post

---
+++ src/main/java/com/sofkaU/PostApp/Post.java
@@ -0,0 +1,38 @@
+package com.sofkaU.PostApp;
+
+import com.sofkaU.PostApp.entity.Comment;
+import lombok.Data;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity(name = "Post")
+@Table(name = "post")
+@Data
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String message;
+    private String title;
+
+    @OneToMany(
+            cascade = CascadeType.ALL,
+            fetch = FetchType.EAGER
+    )
+    private List<Comment> comments = new ArrayList<>();
+
+    public Post addComment(Comment comment){
+        this.comments.add(comment);
+        return this;
+    }
+
+}
---
+++ src/main/java/com/sofkaU/PostApp/entity/Comment.java
@@ -0,0 +1,21 @@
+package com.sofkaU.PostApp.entity;
+
+import lombok.Data;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity(name = "Comment")
+@Table(name = "comment")
+@Data
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String message;
+    private Long fkPostId;
+
+}
